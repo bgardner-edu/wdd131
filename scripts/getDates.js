2e4260724a4eb08ce9f492dce77bfcf3e7a785f3
bgardner-edu <gar19078@byui.edu>
2024-11-06 22:32:44 -0700

    Fixes for intro webpage

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,14 +1,10 @@
-window.onload = function () {
-    const year = document.getElementById("current_year");
-    const lastModified = document.getElementById("last_modified");
+const year = document.getElementById("current_year");
+const lastModified = document.getElementById("last_modified");
 
-    // use the date object
-    const today = new Date();
+const today = new Date();
 
-    year.innerHTML = `<span>${today.getFullYear()}</span>`;
+year.innerHTML = `<span>${today.getFullYear()}</span>`;
 
-    lastModified.innerHTML = `<span>Last Modified: ${new Date(document.lastModified)}</span>`;
+lastModified.innerHTML = `<span>Last Modified: ${new Date(document.lastModified)}</span>`;
 
-
-}
 
