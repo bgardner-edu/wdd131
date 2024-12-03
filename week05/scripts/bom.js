e0ba9445c5fcceaa29ff57ccda52b609a43da69f
bgardner-edu <gar19078@byui.edu>
2024-12-02 20:49:03 -0700

    W05 Prepare activities

---
+++ week05/scripts/bom.js
@@ -0,0 +1,50 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+let chaptersArray = getChapterList() || [];
+
+chaptersArray.forEach(chapter => {
+    displayList(chapter);
+});
+
+button.addEventListener('click', function () {
+    if (input.value.trim() != '') {
+        displayList(input.value);
+
+        chaptersArray.push(input.value);
+        setChapterList();
+        input.value = '';
+        input.focus();
+    }
+})
+
+function displayList(item) {
+    const li = document.createElement('li');
+    li.textContent = item;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = '❌';
+
+    deleteBtn.addEventListener('click', function () {
+        list.removeChild(li);
+        deleteChapter(li.textContent);
+        input.focus();
+    })
+
+    li.appendChild(deleteBtn);
+
+    list.appendChild(li);
+}
+
+function setChapterList(){
+    localStorage.setItem("myFavBOMList", JSON.stringify(chaptersArray));
+}
+
+function getChapterList(){
+    return JSON.parse(localStorage.getItem("myFavBOMList"));
+}
+function deleteChapter(chapter){
+    chapter = chapter.slice(0, chapter.length - 1);
+    chaptersArray = chaptersArray.filter(item => item !== chapter);
+    setChapterList();
+}
