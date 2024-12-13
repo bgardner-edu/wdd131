165de7dab4c547b4bf1a4e132b42281b04f7baea
bgardner-edu <gar19078@byui.edu>
2024-12-13 11:30:54 -0700

    Final Project

---
+++ project/scripts/quotes.js
@@ -0,0 +1,37 @@
+let quotes = loadQuotes();
+displayQuotes();
+
+function loadQuotes() {
+    const q = localStorage.getItem('quotes');
+    return JSON.parse(q) || [];
+}
+
+function saveQuote() {
+    const form = document.getElementById('new_quote')
+    const requestFormData = new FormData(form);
+
+    const data = {};
+    for (let [key, value] of requestFormData.entries()) {
+        data[key] = value;
+    }
+    quotes.push(data);
+    localStorage.setItem('quotes', JSON.stringify(quotes));
+    displayQuotes();
+}
+
+function displayQuotes() {
+    const table = document.getElementById("quotes");
+    const tbody = table.getElementsByTagName("tbody")[0];
+
+    tbody.innerHTML = "";
+
+    quotes.forEach(quote => {
+        let permit = quote['permit'] || "No"
+        const row = tbody.insertRow();
+        row.insertCell().textContent = `${quote['fname']} ${quote['lname']}`;
+        row.insertCell().textContent = `${quote['phone']}`;
+        row.insertCell().textContent = `${quote['type']}`;
+        row.insertCell().textContent = `${quote['completeDate']}`;  
+        row.insertCell().textContent = `${permit}`; 
+    });
+}
